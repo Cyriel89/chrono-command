ac4e169ef81104e87b1d1aa56762505e1520a46b
Cyriel89 <anthony.carrer@hotmail.fr>
2026-01-07 18:55:57 +0100

    feat(frontend): implement real-time clock updates using Signals and RxJS

--- frontend/chrono-front/src/app/clock/clock.ts
+++ frontend/chrono-front/src/app/clock/clock.ts
@@ -1,12 +1,31 @@
-import { DatePipe, TitleCasePipe } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe } from '@angular/common';
+import { Component, Input, OnDestroy, OnInit, signal, WritableSignal } from '@angular/core';
 import { Clock as ClockModel } from '../models/clock.model';
+import { interval, Subscription } from 'rxjs';
 @Component({
   selector: 'app-clock',
-  imports: [TitleCasePipe, DatePipe],
+  imports: [TitleCasePipe, DatePipe, LowerCasePipe],
   templateUrl: './clock.html',
   styleUrl: './clock.scss',
 })
-export class Clock {
+export class Clock implements OnInit, OnDestroy {
   @Input() clock!: ClockModel;
+
+  private timerSubscription!: Subscription;
+  public displayTime: WritableSignal<Date> = signal(new Date());
+
+  ngOnInit(): void {
+    this.displayTime.set(this.setTime());
+    this.timerSubscription = interval(1000).subscribe(() => {
+      this.displayTime.set(this.setTime());
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.timerSubscription.unsubscribe();
+  }
+
+  setTime(): Date {
+    return new Date(new Date().getTime() + (this.clock.timeShift*60*1000));
+  }
 }
